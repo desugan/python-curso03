9cdb8372b7b57e80c7975fa07a7b3d9d0b1626dd
Lucas Souza <106126329+desugan@users.noreply.github.com>
2025-09-19 13:47:36 +0000

    aula 2.10

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ from modelos.cardapio.bebida import Bebida
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
 bebida_suco = Bebida('suco de laranja', 7.50 , 300)
+bebida_suco.aplicar_desconto()
 prato_bife = Prato('bife acebolado', 29.90, 'Bife com cebolas douradas na manteiga')
+prato_bife.aplicar_desconto()
 restaurante_praca.adicionar_no_cardapio(prato_bife)
 restaurante_praca.adicionar_no_cardapio(bebida_suco)
 
--- modelos/cardapio/prato.py
+++ modelos/cardapio/prato.py
@@ -7,3 +7,6 @@ class Prato(ItemCardapio):
     
     def __str__(self):
         return f"{self._nome} - {self.descricao} - R$ {self._preco:.2f}"
+    
+    def aplicar_desconto(self):
+        self._preco -= (self._preco * 0.05)
