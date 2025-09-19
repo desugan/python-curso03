4f22455bb34b9ea27ed509f9a3e67f814b66acd3
Lucas Souza <106126329+desugan@users.noreply.github.com>
2025-09-19 11:41:55 +0000

    aula 2.3

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from modelos.restaurante import Restaurante
-from modelos.cardapio.bebida import Bebida
 from modelos.cardapio.prato import Prato
+from modelos.cardapio.bebida import Bebida
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
-bebida_suco = Bebida('Suco de Melancia', 5.0,'grande')
-prato_paozinho = Prato('Paozinho',2.00,'O melhor pão da cidade')
+bebida_suco = Bebida('suco de laranja', 7.50 , 300)
+prato_bife = Prato('bife acebolado', 29.90, 'Bife com cebolas douradas na manteiga')
+restaurante_praca.adicionar_no_cardapio(prato_bife)
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+
+
 
 def main():
-    print(bebida_suco)
-    print(prato_paozinho)
+    restaurante_praca.exibir_cardapio
 
 if __name__ == '__main__':
     main()
--- modelos/cardapio/bebida.py
+++ modelos/cardapio/bebida.py
@@ -1,9 +1,9 @@
 from modelos.cardapio.item_cardapio import ItemCardapio
 
 class Bebida(ItemCardapio):
-    def __init__(self,nome,preco,tamanho):
-        super().__init__(nome,preco)
+    def __init__(self, nome, preco, tamanho):
+        super().__init__(nome, preco)
         self.tamanho = tamanho
-    
+        
     def __str__(self):
-        return self._nome
+        return f"{self._nome} - {self.tamanho}ml - R$ {self._preco:.2f}"
